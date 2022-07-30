714333b0ba0b78b64a023c4e4cd0d9fb0933ea1b
hpniupi <110271156+hpniupi@users.noreply.github.com>
2022-07-30 10:39:37 +0800

    Add files via upload

---
+++ Project1/Project1/test.c
@@ -0,0 +1,43 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("加如比特\n");
+//	printf("你要好好学习吗？（1/0) >:");
+//	scanf("%d",&input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		//printf("卖红薯\n");
+//		return 0;
+//}
+// #include <stdio.h>
+// #include <string.h>
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	if (num1 > num2)
+//	 printf ("较大值: %d\n", num1);
+//	else
+//		printf("较大值: %d\n", num2);
+//	return 0;
+//}
+ #include <stdio.h>
+ #include <string.h>
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+// }
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = 0;
+//	max = Max(num1, num2);
+//	printf ("max= %d\n", max);
+//	return 0;
+//}
